75dcd9c7de32890f77918b2381f9bba39780ea04
Garuna-A <mishra.anuragdli@gmail.com>
2026-04-30 12:43:20 +0530

    Add time block

--- blank.js
+++ blank.js
@@ -7,3 +7,21 @@ chrome.storage.sync.get('favoriteColor',(data)=>{
         document.body.style.backgroundColor = "#121212";
     }
 });
+
+function UpdateTime(){
+    const now = new Date();
+    const timeString = now.toLocaleTimeString([],{
+        hour:'2-digit',
+        minute:'2-digit',
+        second:'2-digit',
+        hour12:true
+    })
+    const [time, period] = timeString.split(' ');
+
+    const container = document.getElementsByClassName('time-container')[0];
+    if(container){
+        container.innerHTML=`<div>${time}<sup style="font-size: 2rem; margin-left: 10px; margin-top:0px">${period}</sup></div>`
+    }
+}
+UpdateTime();
+setInterval(UpdateTime,1000);
